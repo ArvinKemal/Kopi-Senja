45af8b594d1a8568f1860f4518e032796097ae67
Arvin Kemal <arvin.kema2017@gmail.com>
2023-01-22 17:08:56 +0700

    membuat website kedai kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector('.navbar-nav')
+
+const menu = document.querySelector('#hamburger-menu')
+menu.addEventListener('click', function() {
+    navbarNav.classList.toggle('active')
+})
+
+document.addEventListener('click', function(e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
